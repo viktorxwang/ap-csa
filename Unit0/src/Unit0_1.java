da2961bb2f14cfcd2123ea2c3d63bdc18283063c
Viktor Wang <viktorxwang@gmail.com>
2026-09-09 18:13:02 -0400

    Create Unit0_1.java

---
+++ Unit0/src/Unit0_1.java
@@ -0,0 +1,10 @@
+
+public class Unit0_1 {
+	
+	public static void main(String[] args) {
+		double x = 7 // 2;
+		System.out.println(x);
+		double z = (double) 5 / 2;
+		System.out.println(z);
+	}
+}
